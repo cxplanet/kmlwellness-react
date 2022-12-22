410dcf7b51d8736cc1d2c701b8005a9eaf74a483
Jay Lorenzo <cxplanet@hotmail.com>
2022-12-21 20:17:30 -0800

    Initial reskin

---
+++ src/HomeTitle.js
@@ -0,0 +1,14 @@
+import './HomeTitle.css';
+
+function HomeTitle() {
+    return (
+    <div>
+        <h1 className="heading-primary">
+        <span class="heading-primary--main">KML Wellness</span>
+        <span class="heading-primary--sub">Live healthy, live long</span>
+        </h1>
+    </div>
+    )
+}
+
+export default HomeTitle
