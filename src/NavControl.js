ef9c5151fce85b6da32c3f027b34e7a564ac98b6
MichaelLorenzo05 <michael.lorenzo.2002@gmail.com>
2023-01-16 11:52:46 -0800

    added online class

--- src/NavControl.js
+++ src/NavControl.js
@@ -8,7 +8,8 @@ function NavControl() {
                     <li className="navigation__item"><a href="#" className="navigation__link">About</a></li>
                     <li className="navigation__item"><a href="#" className="navigation__link">Yoga Classes</a></li>
                     <li className="navigation__item"><a href="#" className="navigation__link">Health Coaching</a></li>
-                    <li className="navigation__item"><a href="#" class="navigation__link">Videos</a></li>
+                    <li className="navigation__item"><a href="#" className="navigation__link">Online Classes</a></li>
+                    <li className="navigation__item"><a href="#" className="navigation__link">Gallery</a></li>
                     <li className="navigation__item"><a href="#" className="navigation__link">Contact</a></li>
                     </ul>
             </nav>
