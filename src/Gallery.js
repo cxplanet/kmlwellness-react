d472cf4424b943ef8e8b9da2841e816876f7a90e
MichaelLorenzo05 <michael.lorenzo.2002@gmail.com>
2023-01-23 17:05:35 -0800

    Gallery picture dsiplay

--- src/Gallery.js
+++ src/Gallery.js
@@ -4,13 +4,16 @@ import { useState } from 'react';
 function YogaGallery() {
     const [galleryCell, setGalleryCell]=useState();
     const cellClick= (event) => {
+        console.log(event);
         const { id } = event.target;
         alert(id);
         //setGalleryCell(cellNumber);
     }
     return (
       <div className="grid-container">
-        <div id="1" className="grid-item" onClick={cellClick}>1</div>
+        <div className="grid-item" onClick={cellClick}>
+            <img id="1" src="https://www.kml-wellness.com/img/ypose4.jpg" alt="ypose4" />
+        </div>
         <div id="2" className="grid-item">2</div>
         <div className="grid-item">3</div>
         <div className="grid-item">4</div>
