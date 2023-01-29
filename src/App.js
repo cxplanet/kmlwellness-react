9575f3145b9efe35ed27d1cbdf766d171d5e0daf
MichaelLorenzo05 <michael.lorenzo.2002@gmail.com>
2023-01-28 17:52:27 -0800

    router disaster

--- src/App.js
+++ src/App.js
@@ -2,28 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 import './HomeTitle';
 import HomeTitle from './HomeTitle';
-import './NavControl';
-import NavControl from './NavControl';
+// import './NavControl';
+// import NavControl from './NavControl';
 import Schedule from './Schedule';
 import YogaGallery2 from './Gallery2';
+import { Routes, Route, Link } from "react-router-dom";
+import './NavControl.css';
 
 function App() {
   return (
-    <div 
+    <div
       className="App">
-      <header className="App-header">
-        <div className="navigation">
+      {/* <header className="App-header">
+        {/* <div className="navigation">
           <NavControl />
-        </div>
-      </header>
-      <div
-          className="App-content">
-          {/*<HomeTitle className="heading-primary"/>*/}
-          {/*<Schedule />*/}
-          <YogaGallery2/>
-      </div>
-    </div>
+        </div> */}
+      {/* </header> */}
+      
+        <Routes>
+          <Route path="/" element={<NavControl />}>
+            <Route index element={<HomeTitle />} />
+            <Route path="classes" element={<Schedule />}></Route>
+            <Route path="gallery" element={<YogaGallery2 />}></Route>
+            <Route path="*" element={<HomeTitle />}></Route>
+          </Route>
+        </Routes>
+      
+    </div >
   );
+}
+
+function NavControl() {
+  return(
+      <div className="nav_menu">
+          <nav className="navigation__nav">
+              <ul className="navigation__list">
+                  <li className="navigation__item"><Link to="/">Home</Link></li>
+                  <li className="navigation__item"> <Link to="/about">About</Link></li>
+                  <li className="navigation__item"> <Link to="/classes">Yoga Classes</Link></li>
+                  <li className="navigation__item"> <Link to="/coaching">Health Coaching</Link></li>
+                  <li className="navigation__item"> <Link to="/online">Online Classes</Link></li>
+                  <li className="navigation__item"> <Link to="/gallery">Gallery</Link></li>
+                  <li className="navigation__item"> <Link to="/contact">Contact</Link></li>
+              </ul>
+          </nav>
+      </div>
+  )
 }
 
 export default App;
--- src/NavControl.js
+++ src/NavControl.js
@@ -1,17 +1,19 @@
 import './NavControl.css';
+import { Link } from "react-router-dom";
 
 function NavControl() {
     return(
         <div className="nav_menu">
             <nav className="navigation__nav">
                 <ul className="navigation__list">
-                    <li className="navigation__item"><a href="#" className="navigation__link">About</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link">Yoga Classes</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link">Health Coaching</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link">Online Classes</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link">Gallery</a></li>
-                    <li className="navigation__item"><a href="#" className="navigation__link">Contact</a></li>
-                    </ul>
+                    <li className="navigation__item"><Link to="/">Home</Link></li>
+                    <li className="navigation__item"> <Link to="/about">About</Link></li>
+                    <li className="navigation__item"> <Link to="/classes">Yoga Classes</Link></li>
+                    <li className="navigation__item"> <Link to="/coaching">Health Coaching</Link></li>
+                    <li className="navigation__item"> <Link to="/online">Online Classes</Link></li>
+                    <li className="navigation__item"> <Link to="/gallery">Gallery</Link></li>
+                    <li className="navigation__item"> <Link to="/contact">Contact</Link></li>
+                </ul>
             </nav>
         </div>
     )
