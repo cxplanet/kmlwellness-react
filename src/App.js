05b105fcef5cc1b3a5d3d720aa9a710c1e4fc429
MichaelLorenzo05 <michael.lorenzo.2002@gmail.com>
2023-01-29 14:18:14 -0800

    links routed

--- src/App.js
+++ src/App.js
@@ -8,19 +8,13 @@ import Schedule from './Schedule';
 import YogaGallery2 from './Gallery2';
 import { Routes, Route, Link } from "react-router-dom";
 import './NavControl.css';
+import AppLayout from './AppLayout';
 
 function App() {
   return (
-    <div
-      className="App">
-      {/* <header className="App-header">
-        {/* <div className="navigation">
-          <NavControl />
-        </div> */}
-      {/* </header> */}
-      
+    <div>
         <Routes>
-          <Route path="/" element={<NavControl />}>
+          <Route path="/" element={<AppLayout />}>
             <Route index element={<HomeTitle />} />
             <Route path="classes" element={<Schedule />}></Route>
             <Route path="gallery" element={<YogaGallery2 />}></Route>
@@ -30,24 +24,6 @@ function App() {
       
     </div >
   );
-}
-
-function NavControl() {
-  return(
-      <div className="nav_menu">
-          <nav className="navigation__nav">
-              <ul className="navigation__list">
-                  <li className="navigation__item"><Link to="/">Home</Link></li>
-                  <li className="navigation__item"> <Link to="/about">About</Link></li>
-                  <li className="navigation__item"> <Link to="/classes">Yoga Classes</Link></li>
-                  <li className="navigation__item"> <Link to="/coaching">Health Coaching</Link></li>
-                  <li className="navigation__item"> <Link to="/online">Online Classes</Link></li>
-                  <li className="navigation__item"> <Link to="/gallery">Gallery</Link></li>
-                  <li className="navigation__item"> <Link to="/contact">Contact</Link></li>
-              </ul>
-          </nav>
-      </div>
-  )
 }
 
 export default App;
