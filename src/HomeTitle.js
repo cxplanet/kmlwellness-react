0202df2ff568d8ce0cc5ae4e5f26dfe34f7bbffb
MichaelLorenzo05 <michael.lorenzo.2002@gmail.com>
2023-03-06 17:06:55 -0800

    event times

--- src/HomeTitle.js
+++ src/HomeTitle.js
@@ -4,8 +4,8 @@ function HomeTitle() {
     return (
     <div>
         <h1 className="heading-primary">
-        <span class="heading-primary--main">KML Wellness</span>
-        <span class="heading-primary--sub">Live healthy, live long</span>
+        <span className="heading-primary--main">KML Wellness</span>
+        <span className="heading-primary--sub">Live healthy, live long</span>
         </h1>
     </div>
     )
--- src/Schedule.js
+++ src/Schedule.js
@@ -1,15 +1,24 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import rrulePlugin from '@fullcalendar/rrule';
+import './Schedule.css';
 
 const events = [
   { title: 'Stationary Posture', 
-    daysOfWeek: [ 1 ] }
+   startTime: '18:00',
+   endTime: '19:30',
+   eventDisplay: 'auto',
+   daysOfWeek: [ 1 ] },
+  { title: 'Stationary Posture', 
+   startTime: '16:15',
+   endTime: '17:30',
+   eventDisplay: 'auto',
+   daysOfWeek: [ 3 ] }
 ]
 
 export function Schedule() {
   return (
-    <div>
+    <div className='schedule'>
       <h1>Yoga Class Schedule</h1>
       <FullCalendar
         plugins={[rrulePlugin, dayGridPlugin]}
